d38b6a40e983d602e2aa73cf08f5056eca262e51
sajib <sajibcse28@gmail.com>
2019-07-22 00:55:32 +0600

    A function added called greating in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 class Hello{
 public static void main(string[] args){
 System.out.println("Vs Code is awesome");
+greating("sajib Biswas");
 }
-    
+public function greating(string name){
+System.out.println("Welcome"+name);
+}   
 }
