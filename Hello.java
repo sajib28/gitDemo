84deec19b018a91280b32e72884868c4ae74d67d
sajib <sajibcse28@gmail.com>
2019-07-22 16:37:04 +0600

    I have added a file and chnage Hello.java file

--- Hello.java
+++ Hello.java
@@ -6,4 +6,7 @@ System.out.println("Vs Code is awesome");
 public void function message(){
     System.out.println("Hurrah! we are able to contribute on open source");
 }
+public int function add(int a, int b){
+return a+b;
+}
 }
