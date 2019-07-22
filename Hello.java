bcdf6fe4bc308798dc8923a9752a98409cdfa6be
sajib <sajibcse28@gmail.com>
2019-07-22 17:01:54 +0600

    Delete previous method and added new method called sqrt

--- Hello.java
+++ Hello.java
@@ -3,10 +3,7 @@ public static void main(string[] args){
 System.out.println("Vs Code is awesome");
 
 }
-public void function message(){
-    System.out.println("Hurrah! we are able to contribute on open source");
-}
-public int function add(int a, int b){
-return a+b;
-}
+public static void strt(int x){
+    return x*x;
+
 }
